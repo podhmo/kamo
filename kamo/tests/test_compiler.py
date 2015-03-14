5de92db021e500f97a61ce1b84accb2005248d8e
podhmo <podhmo+altair@beproud.jp>
2015-03-15 03:13:11 +0900

    add test

---
+++ kamo/tests/test_compiler.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+import unittest
+from evilunit import test_target
+
+
+@test_target("kamo:Compiler")
+class Tests(unittest.TestCase):
+    def _callFUT(self, code):
+        from kamo import (
+            Lexer,
+            Parser
+        )
+        lexer = Lexer()
+        parser = Parser()
+        compiler = self._getTarget()()
+        return compiler(parser(lexer(code)))
+
+    def test_it0(self):
+        code = """
+${datetime.now()}
+"""
+        result = str(self._callFUT(code))
+        self.assertIn("c['datetime']", result)
+
+    def test_it1(self):
+        code = """
+<% from datetime import datetime %>
+${datetime.now()}
+"""
+        result = str(self._callFUT(code))
+        self.assertNotIn("c['datetime']", result)
+
+    def test_it2(self):
+        code = """
+<%datetime = object()%>
+from datetime import datetime
+${datetime.now()}
+"""
+        result = str(self._callFUT(code))
+        self.assertNotIn("c['datetime']", result)
+
+    def test_it2(self):
+        code = """
+%for datetime in xs:
+    ${datetime.now()}
+%endfor
+"""
+        result = str(self._callFUT(code))
+        self.assertNotIn("c['datetime']", result)
