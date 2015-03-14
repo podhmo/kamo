e67fc341d789dc2775c8920b2a2ab1261b1a3eb2
podhmo <podhmo+altair@beproud.jp>
2015-03-14 23:11:55 +0900

    add demo

---
+++ demo/demo.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import logging
+from minimako import Template
+# logging.basicConfig(level=logging.DEBUG)
+
+
+template = Template("""
+<%
+def decorate(s):
+    return "** {} **".format(s)
+%>
+<%from datetime import datetime%>
+
+${greeting|decorate}
+${name}: this is my first sample! (now: ${datetime.now()})
+""")
+
+
+print(template.render(name="foo", greeting="chears"))
+print("----------------------------------------")
+print(template.render(name="boo", greeting="chears"))
